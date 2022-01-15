82f39afae439015a5a942f4a094536f6c4b367ec
Zethange <85435455+Zethange@users.noreply.github.com>
2022-01-15 16:20:13 +0300

    update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – S.T.A.L.K.E.R. Mobile',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Новости и описание мобильных проектов
       </span>
     </>
   ),
