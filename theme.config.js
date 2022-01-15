03895a2fe4a14762d472323420d877166a3965ba
Zethange <85435455+Zethange@users.noreply.github.com>
2022-01-15 16:38:31 +0300

    update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <> {new Date().getFullYear()} © Неравнодушный сталкер.</>,
   unstable_faviconGlyph: '👋',
 }
