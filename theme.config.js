c2bdc8a31026d4812d8b10bdcc0b95984179fce1
Zethange <85435455+Zethange@users.noreply.github.com>
2022-01-16 17:13:24 +0300

    update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -14,8 +14,8 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="S.T.A.L.K.E.R. Mobile News" />
+      <meta name="og:description" content="S.T.A.L.K.E.R. Mobile News" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
